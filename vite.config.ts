be82919c9683f705c1dd8ad52152c7df346a1016
Anjan Shomodder <thatanjan@gmail.com>
2024-08-19 14:16:41 +0600

    Use 3000 as port

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [react(), tsconfigPaths()],
   build: {
     lib: {
